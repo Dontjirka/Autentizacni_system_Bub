6b5bed26f276c23d50fccde61ddf9d999a1733c1
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2022-12-04 09:52:12 +0100

    Update hello.php

--- novy_ukol/hello.php
+++ novy_ukol/hello.php
@@ -3,7 +3,7 @@ require('login.php');
 ?>
 <!DOCTYPE html>
 <head>
-    <title>Nevim</title>
+    <title>Error</title>
     <meta charset='utf-8'>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
