55fc8061c0ec68bbe2b0d3d24b43b62fe0ab265c
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2022-12-04 09:52:41 +0100

    Update login.php

--- novy_ukol/login.php
+++ novy_ukol/login.php
@@ -5,7 +5,7 @@ session_start();
 <!DOCTYPE html>
 
 <head>
-    <title>Nevim</title>
+    <title>Login</title>
     <meta charset='utf-8'>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
